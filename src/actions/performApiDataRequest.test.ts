13ff2d86aaffd57dd6f16c9186eccdf22d721336
Gijs <gijs@oberon.nl>
2019-01-21 15:05:57 +0100

    ->

--- src/actions/performApiDataRequest.test.ts
+++ src/actions/performApiDataRequest.test.ts
@@ -2,9 +2,11 @@ import { getState } from '../mocks/mockActions';
 import { performApiRequest } from './performApiDataRequest';
 import thunk from 'redux-thunk';
 import configureMockStore from 'redux-mock-store';
+import {apiDataFail} from "./apiDataFail";
 
 // TODO:
 // - reject wanneer geen config is geset.
+
 // - check of request functie wel/niet wordt aangeroepen. (aan de hand van cachduration)
 // - Wanneer functie gecalled wordt, checken welke acties worden gedispatched (verschillende response statussen/ reject).
 // - check of callbacks uit de config worden aangeroepen.
@@ -24,47 +26,64 @@ const defaultState = {
     }
 };
 
-// const response1 = {
-//     body: { data: 'json' },
-//     ok: true,
-//     redirected: false,
-//     status: 200,
-//     statusText: "ok"
-// };
 const middlewares = [thunk];
 const mockStore = configureMockStore(middlewares);
 
 const dispatch = jest.fn();
+const request = jest.fn();
+// const apiDataFail = jest.fn();
 
-jest.mock('../request', () => {
+const mockPromise = (mockBody: object, mockResponse: object) => {
     return () => new Promise((resolve, reject) => {
         process.nextTick(() => {
-            return resolve(1);
+            return resolve({
+                response: mockResponse,
+                body: mockBody
+            });
         })
     });
-});
+};
 
-test('Empty config', () => {
+test('It gets an error message when config is empty', () => {
     const store: any = mockStore(defaultState);
-    return expect(performApiRequest('postData/', {}, { data: 'json' })(dispatch, () => store.getState())).rejects.toBeTruthy();
+    return expect(performApiRequest('postData/', {}, {data: 'json'})(dispatch, () => store.getState())).rejects.toBe('apiData.performApiRequest: no config with key postData/ found!');
 });
 
 test('The request status or params has been changed', () => {
+    request.mockClear();
+
+    request.mockReturnValue(
+        mockPromise({}, {ok: true})
+    );
+
     const initialState = { apiData: getState('postData', {}, 'ready', 'POST') };
     const store: any = mockStore(initialState);
-    return expect(performApiRequest('postData/', {}, { data: 'json' })(dispatch, () => store.getState())).resolves.toBeTruthy();
+    return expect(performApiRequest('postData/', {}, { data: 'json' })(dispatch, () => store.getState())).resolves.toBeUndefined();
 });
 
+test('It rejects when response.ok is false', () => {
+    request.mockClear();
 
+    request.mockReturnValue(
+        mockPromise({}, {ok: false})
+    );
 
+    const body = {
+        // body: ReadableStream | null;
+        // headers: Headers;
+        // ok: boolean;
+        // redirected: boolean;
+        // status: number;
+        // statusText: string;
+        // type: ResponseType;
+        // url: string;
+        // clone(): Response;
+    }
+    
+    const initialState = { apiData: getState('postData', {}, 'ready', 'POST') };
+    const store: any = mockStore(initialState);
 
-// test('It sets the request properties correctly', () => {
-//     const bodyOne = {};
-//     const apiDataStateOne = getState(bindings, {}, 'ready', 'POST');
-//     const testOne = getRequestProperties(apiDataStateOne.endpointConfig, apiDataStateOne.globalConfig, apiDataStateOne, bodyOne);
-//     const propertiesOne = { body: '{}',
-//         headers: { 'Content-Type': 'application/json' },
-//         method: 'POST' };
-//
-//     expect(testOne.toEqual(propertiesOne));
-// });
+    performApiRequest('postData/', {}, body)(dispatch, () => store.getState());
+
+    return expect(dispatch).toHaveBeenCalledWith(apiDataFail('postData', body, body));
+});
