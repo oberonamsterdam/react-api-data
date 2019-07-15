811c876990ac0807375e4680897caa60a021916a
Rik Scheffer <rik@oberon.nl>
2019-07-15 16:54:06 +0200

    url encode params

--- src/helpers/formatUrl.ts
+++ src/helpers/formatUrl.ts
@@ -1,4 +1,4 @@
 import { EndpointParams } from '../index';
 
 export const formatUrl = (url: string, params?: EndpointParams): string =>
-    !params ? url : url.replace(/:[a-zA-Z]+/g, match => params ? String(params[match.substr(1)]) || '' : '');
+    !params ? url : url.replace(/:[a-zA-Z]+/g, match => params ? encodeURIComponent(String(params[match.substr(1)])) || '' : '');
