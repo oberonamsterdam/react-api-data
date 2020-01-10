01b441340a638f8e2b5b98cd47e02220407d6923
Richard van Willegen <rvw@oberon.nl>
2020-01-10 16:25:09 +0100

    Fixed logic of useApiData to match the (intended) logic of withApiData. Tests needed.

---
+++ src/useApiData.test.ts
@@ -0,0 +1,20 @@
+// import { renderHook, act } from '@testing-library/react-hooks'
+// import useApiData from './useApiData';
+
+test('useApiData should trigger perform under the right conditions', () => {
+    // todo: test these cases here:
+    // the hook should call perform when shouldAutoTrigger and:
+    // - the component gets mounted
+    // - the params have changed
+    // - the endpoint has changed
+    // - the call has been invalidated (networkStatus is ready)
+
+    // const params1 = {};
+    // const { result } = renderHook(() => useApiData('testEndpoint', params1));
+    // const perform = jest.fn(() => {});
+    // result.current.perform = perform;
+    //
+    // act(() => {
+    //     expect(perform).toHaveBeenCalled();
+    // })
+});
--- src/useApiData.ts
+++ src/useApiData.ts
@@ -1,4 +1,4 @@
-import { useRef } from 'react';
+import {useEffect, useRef} from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { ApiDataBinding, ApiDataState, EndpointParams } from './index';
 import { BindingsStore } from './helpers/createApiDataBinding';
@@ -6,18 +6,35 @@ import { shouldAutoTrigger } from './withApiData';
 
 type UseApiDataHook = <T>(endpointKey: string, params?: EndpointParams, instanceId?: string) => ApiDataBinding<T>;
 
+// the hook should call perform when shouldAutoTrigger and:
+// - the component gets mounted
+// - the params have changed
+// - the endpoint has changed
+// - the call has been invalidated (networkStatus is ready)
+
 const useApiData: UseApiDataHook = <T>(endpointKey: string, params?: EndpointParams, instanceId: string = '') => {
     const bindingsStore = useRef<BindingsStore>(new BindingsStore());
+    const prevParams = useRef<EndpointParams>();
+    const prevEndpointKey = useRef<string>();
     const apiData: ApiDataState = useSelector((state: { apiData: ApiDataState }) => state.apiData);
     const autoTrigger = shouldAutoTrigger(apiData, endpointKey);
     const dispatch = useDispatch();
     const binding: ApiDataBinding<T> = bindingsStore.current.getBinding(endpointKey, params, dispatch, instanceId, apiData);
-    const request = binding.request;
-    const networkStatus = request && request.networkStatus;
+    const networkStatus = binding.request.networkStatus;
 
-    if (autoTrigger && networkStatus === 'ready') {
-        binding.perform(params);
-    }
+    useEffect(() => {
+        if (
+            !autoTrigger && (
+                prevParams.current !== params ||
+                prevEndpointKey.current !== endpointKey ||
+                networkStatus === 'ready'
+            )
+        ) {
+            prevParams.current = params;
+            prevEndpointKey.current = endpointKey;
+            binding.perform(params);
+        }
+    }, [autoTrigger, params, endpointKey, networkStatus]);
 
     return binding;
 };
