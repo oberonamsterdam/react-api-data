ff7066373bd7ffbaa439b46ac566608ecc5108ae
Jesse <jesse.vanderheijden@gmail.com>
2020-03-02 17:42:41 +0100

    clean up

--- src/useApiData.test.ts
+++ src/useApiData.test.ts
@@ -50,42 +50,32 @@ describe('useApiData should trigger perform under the right conditions', () => {
     });
 
     it('performs a request when status is ready and does not when hook got rerendered with same endpoint && params and status not ready', async () => {
-        const { wait, waitForNextUpdate, rerender } = renderHook(({ endpointKey, params }) => useApiData(endpointKey, params),
-                                                                 { initialProps: { endpointKey: 'testEndpoint', params: {} } }
+        const { rerender } = renderHook(({ endpointKey, params }) => useApiData(endpointKey, params),
+                                        { initialProps: { endpointKey: 'testEndpoint', params: {} } }
             );
-        await wait(() => undefined);
-        expect(performMock1).toHaveBeenCalled();
-        await waitForNextUpdate;
+        expect(performMock1).toHaveBeenCalledTimes(1);
         rerender();
-        await wait(() => undefined);
         expect(performMock1).toHaveBeenCalledTimes(1);
     }, 3000
     );
 
     it('performs a request when status is ready and also when hook got rerenderd with different params', async () => {
-        const { wait, waitForNextUpdate, rerender } = renderHook(({ endpointKey, params }) => useApiData(endpointKey, params),
-                                                                 { initialProps: { endpointKey: 'testEndpoint', params: {} } }
+        const { rerender } = renderHook(({ endpointKey, params }) => useApiData(endpointKey, params),
+                                        { initialProps: { endpointKey: 'testEndpoint', params: {} } }
             );
-        await wait(() => undefined);
-        expect(performMock1).toHaveBeenCalled();
-        await waitForNextUpdate;
-        rerender();
-        await wait(() => undefined);
+        expect(performMock1).toHaveBeenCalledTimes(1);
         rerender({ endpointKey: 'testEndpoint', params: { param: 1 } });
         expect(performMock1).toHaveBeenCalledTimes(2);
     }, 3000
     );
 
     it('performs a request when status is ready and autotrigger true and when endpoint name changes it does a new perform', async () => {
-        const { wait, waitForNextUpdate, rerender } = renderHook(({ endpointKey, params }) => useApiData(endpointKey, params),
-                                                                 { initialProps: { endpointKey: 'testEndpoint', params: {} } }
+        const { rerender } = renderHook(({ endpointKey, params }) => useApiData(endpointKey, params),
+                                        { initialProps: { endpointKey: 'testEndpoint', params: {} } }
             );
-
-        await wait(() => undefined);
-        expect(performMock1).toHaveBeenCalled();
+        expect(performMock1).toHaveBeenCalledTimes(1);
         const performMock2 = jest.fn();
         mockBindingWithStore(store1, bindings[1].name, performMock2, getBinding);
-        await waitForNextUpdate;
 
         rerender({ endpointKey: 'newEndpoint', params: {} });
         expect(performMock2).toHaveBeenCalledTimes(1);
@@ -94,12 +84,11 @@ describe('useApiData should trigger perform under the right conditions', () => {
     );
     it('performs a request when status is ready and autotrigger true and rerenders after invalidation', async () => {
         mockBindingWithStore(store1, endpointKey1, performMock1, getBinding);
-        const { waitForNextUpdate, rerender } = renderHook(({ endpointKey, params }) => useApiData(endpointKey, params),
-                                                           { initialProps: { endpointKey: 'testEndpoint', params: {} } }
+        const { rerender } = renderHook(({ endpointKey, params }) => useApiData(endpointKey, params),
+                                        { initialProps: { endpointKey: 'testEndpoint', params: {} } }
             );
-        await waitForNextUpdate;
         expect(performMock1).toHaveBeenCalled();
-            // fake invalidations by tweaking the result values
+        // set ready tot success to make a new store to test the invalidations.
         const newBindings: ConfigureBinding[] = [
                 { name: 'testEndpoint', params: {}, networkStatus: 'success', method: 'GET' },
                 { name: 'newEndpoint', params: {}, networkStatus: 'ready', method: 'GET' },
@@ -111,15 +100,14 @@ describe('useApiData should trigger perform under the right conditions', () => {
             useDispatch: () => store2.dispatch
         }));
         mockBindingWithStore(store2, endpointKey1, performMock1, getBinding);
-        await waitForNextUpdate;
         rerender();
         expect(performMock1).toHaveBeenCalledTimes(1);
+        // configure the old store with status ready to see or it will retrigger.
         jest.mock('react-redux', () => ({
             useSelector: (fn: any) => fn(store1.getState()),
             useDispatch: () => store1.dispatch
         }));
         mockBindingWithStore(store1, endpointKey1, performMock1, getBinding);
-        await waitForNextUpdate;
         rerender();
         expect(performMock1).toHaveBeenCalledTimes(2);
     }, 3000
