ef3b3ca3b28cb220b7075bb2766da5970731a246
Jari Zwarts <jari@oberon.nl>
2020-10-12 11:23:05 +0200

    fix(ssr): move 'useSSR' param to new hook options object.

--- src/useApiData.ts
+++ src/useApiData.ts
@@ -1,6 +1,6 @@
 import { useEffect, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Binding, EndpointParams, EndpointConfig } from './types';
+import { Binding, EndpointConfig, EndpointParams } from './types';
 import { BindingsStore } from './helpers/createBinding';
 import { shouldAutoTrigger } from './withApiData';
 import { State } from './reducer';
@@ -8,6 +8,7 @@ import shallowEqual from 'shallowequal';
 
 export interface HookOptions extends Partial<EndpointConfig> {
     instanceId?: string;
+    isSSR?: boolean;
 }
 
 type UseHook = <T, F = unknown>(endpointKey: string, params?: EndpointParams, options?: HookOptions) => Binding<T, F>;
@@ -18,17 +19,25 @@ type UseHook = <T, F = unknown>(endpointKey: string, params?: EndpointParams, op
 // - the endpoint has changed
 // - the call has been invalidated (networkStatus is ready)
 
-const useApiData: UseHook = <T, F = unknown>(endpointKey: string, params?: EndpointParams, options?: HookOptions,
-                                             // we auto detect a SSR environment. If we are on SSR, we will immediately execute the request during every render(!)
-                                             isSSR: boolean = typeof document === 'undefined') => {
-    const { instanceId, ...config } = options ?? {};
+const useApiData: UseHook = <T, F = unknown>(endpointKey: string, params?: EndpointParams, options?: HookOptions) => {
+    const {
+        instanceId,
+        // we auto detect a SSR environment. If we are on SSR, we will immediately execute the request during every render(!)
+        isSSR = typeof document === 'undefined',
+        ...config
+    } = options ?? {};
     const bindingsStore = useRef<BindingsStore>(new BindingsStore());
     const prevParams = useRef<EndpointParams>();
     const prevEndpointKey = useRef<string>();
     const apiData: State = useSelector((state: { apiData: State }) => state.apiData);
     const autoTrigger = shouldAutoTrigger(apiData, endpointKey);
     const dispatch = useDispatch();
-    const binding: Binding<T, F> = bindingsStore.current.getBinding(endpointKey, params, dispatch, instanceId, apiData,
+    const binding: Binding<T, F> = bindingsStore.current.getBinding(
+        endpointKey,
+        params,
+        dispatch,
+        instanceId,
+        apiData,
         undefined,
         config
     );
